eea389976af9f8b0784a3e6a42fa5dc7a2d9ac57
meeksfred <maxfred0025@gmail.com>
2016-03-08 15:32:51 -0800

    finish article methods, stylize

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -63,18 +63,17 @@ articleView.handleMainNav = function() {
 articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
 
-  // TODO: Add an event handler to reveal all the hidden elements,
-  //       when the .read-on link is clicked. You can go ahead and hide the
-  //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
-  //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
-  //       process any .read-on clicks that happen within child nodes.
-
+  $('#articles').on('click', '.read-on', function(event) {
+    $(this).parent().find('*').show();
+    event.preventDefault();
+    $(this).hide();
+  });
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function () {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 });
