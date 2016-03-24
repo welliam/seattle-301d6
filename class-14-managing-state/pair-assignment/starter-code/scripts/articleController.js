893391b8ef87b437f35a823ecef675a236a6ef3a
Justin Lange <well1912@gmail.com>
2016-03-24 12:48:59 -0700

    Unzip class 14 starter-code, comment two methods

---
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -0,0 +1,68 @@
+(function(module) {
+  var articlesController = {};
+
+  Article.createTable();  // Ensure the database table is properly initialized
+
+  articlesController.index = function(ctx, next) {
+    articleView.index(ctx.articles);
+  };
+
+  // DONE: What does this method do? What is it's execute path?
+  // This method loads the articles by id in reference to the id in
+  // the URL. It defines a callback called articleData, then gives it
+  // to findWhere along with the SQL id 'id', and the id from the URL
+  // the user navigated to. After findWhere finds the corresponding
+  // article, it executes the function we defined, which sets the
+  // articles field of the contect object to the found article and
+  // calls `next`.
+  articlesController.loadById = function(ctx, next) {
+    var articleData = function(article) {
+      ctx.articles = article;
+      next();
+    };
+
+    Article.findWhere('id', ctx.params.id, articleData);
+  };
+
+  // COMMENT: What does this method do?  What is it's execution path?
+  // This method loads the articles based on the author's name. It
+  // proceeds with the article findWhere as in the above example, but
+  // with here we also replaces +s with spaces in the authorName URL
+  // parameter.
+  articlesController.loadByAuthor = function(ctx, next) {
+    var authorData = function(articlesByAuthor) {
+      ctx.articles = articlesByAuthor;
+      next();
+    };
+
+    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
+  };
+
+  // COMMENT: What does this method do?  What is it's execution path?
+  articlesController.loadByCategory = function(ctx, next) {
+    var categoryData = function(articlesInCategory) {
+      ctx.articles = articlesInCategory;
+      next();
+    };
+
+    Article.findWhere('category', ctx.params.categoryName, categoryData);
+  };
+
+  // COMMENT: What does this method do?  What is it's execution path?
+  articlesController.loadAll = function(ctx, next) {
+    var articleData = function(allArticles) {
+      ctx.articles = Article.all;
+      next();
+    };
+
+    if (Article.all.length) {
+      ctx.articles = Article.all;
+      next();
+    } else {
+      Article.fetchAll(articleData);
+    }
+  };
+
+
+  module.articlesController = articlesController;
+})(window);
